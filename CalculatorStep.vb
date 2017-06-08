9ab602284eff8e544373ce5aca1586305dc630c6
Dj <djones.uk63@gmail.com>
2017-06-08 09:58:34 +0100

    CalculatorStep.vb
    An enumeration which will allow us to track what stage we are on calculating our final UCAS grade.

---
+++ CalculatorStep.vb
@@ -0,0 +1,29 @@
+''' <license> 
+''' Copyright (C) 2017  Dominick Jones
+''' 
+''' This program is free software: you can redistribute it and/or modify
+''' it under the terms of the GNU General Public License as published by 
+''' the Free Software Foundation, either version 3 of the License, or
+''' any later version.
+''' 
+''' This program is distributed in the hope that it will be useful,
+''' but WITHOUT ANY WARRANTY; without even the implied warranty of
+''' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+''' GNU General Public License for more details.
+''' 
+''' You should have received a copy of the GNU General Public License 
+''' along with this program. If not, see <a>http://www.gnu.org/licenses/"</a>.
+''' </license>
+''' 
+''' <summary>
+''' Different steps of the calculating steps.
+''' </summary>
+Public Enum CalculatorStep
+
+    SAVE_COURSE
+    POINTS_ENTERED_P
+    POINTS_ENTERED_M
+    POINTS_ENTERED_D
+    POINTS_ENTERED_NP
+    CALCULATED
+End Enum
